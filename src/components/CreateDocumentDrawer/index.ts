5d66ae1c344cffd8769544f8c4654af8003bbb6d
Dmitri.Astafiev <dmitri.astafiev@maib.md>
2026-07-13 20:46:51 +0300

    refactor: remove CreateDocumentDrawer component in favor of dedicated document edit page

--- src/components/CreateDocumentDrawer/index.ts
+++ src/components/CreateDocumentDrawer/index.ts
@@ -1,5 +1,3 @@
-export { CreateDocumentDrawer } from './CreateDocumentDrawer';
-export type { CreateDocumentDrawerProps } from './CreateDocumentDrawer';
 export { DocumentFormField } from './DocumentFormField';
 export { RelationField } from './RelationField';
 export * from './helpers';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,1 @@
 export { ErrorBoundary } from './ErrorBoundary';
-export { CreateDocumentDrawer } from './CreateDocumentDrawer/CreateDocumentDrawer';
-export type { CreateDocumentDrawerProps } from './CreateDocumentDrawer/CreateDocumentDrawer';
